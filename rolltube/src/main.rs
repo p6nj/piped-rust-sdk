e6a20cc5af0ca3db1658813795fe18b45480a1cd
p6nj <breee@duck.com>
2024-08-07 11:36:32 +0200

    remove some dust

--- rolltube/src/main.rs
+++ rolltube/src/main.rs
@@ -5,19 +5,18 @@ use reqwest::Client;
 
 #[tokio::main]
 async fn main() -> piped::Result<()> {
-    Ok({
-        let client = Client::new();
-        let piped = PipedClient::new(
-            &client,
-            env::var("INSTANCE")
-                .map(Cow::Owned)
-                .unwrap_or(Cow::Borrowed("https://pipedapi.kavin.rocks")),
-        );
-        dbg!(
-            piped
-                .playlist_from_id("PLvVUc0cy2yXgVI8Dod5Uck-yiXoRAw_yX")
-                .await?
-                .name
-        );
-    })
+    let client = Client::new();
+    let piped = PipedClient::new(
+        &client,
+        env::var("INSTANCE")
+            .map(Cow::Owned)
+            .unwrap_or(Cow::Borrowed("https://pipedapi.kavin.rocks")),
+    );
+    dbg!(
+        piped
+            .playlist_from_id("PLvVUc0cy2yXgVI8Dod5Uck-yiXoRAw_yX")
+            .await?
+            .name
+    );
+    Ok(())
 }
